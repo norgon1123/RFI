3085af4c5ecee490119de49105b31a8c5adc6c84
Neil Orgon <neil.orgon@gmail.com>
2017-07-26 15:16:16 -0700

    User can cycle through procore images using tap gesture

---
+++ Assets/CycleImages.cs
@@ -0,0 +1,33 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleImages : MonoBehaviour, IInputClickHandler
+{
+    public Material[] images;
+
+    private int count;
+    private new Renderer renderer;
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        count++;
+        renderer.material = images[count];
+        if (count == 4)
+        {
+            count = -1;
+        }
+    }
+}
